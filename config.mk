c0f2e64debbc134f8082ac7c8c66e4f65fde27f8
Dan <221143711@student.uj.ac.za>
2024-01-01 22:27:25 +0200

    Configure to work with FreeBSD

--- config.mk
+++ config.mk
@@ -5,10 +5,10 @@ VERSION = 1.0
 
 # paths
 PREFIX = /usr/local
-MANPREFIX = $(PREFIX)/share/man
+MANPREFIX = $(PREFIX)/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = ${PREFIX}/include
+X11LIB =${PREFIX}/lib
 
 # flags
 CPPFLAGS = -I$(X11INC) -D_DEFAULT_SOURCE -DVERSION=\"${VERSION}\"
@@ -16,7 +16,7 @@ CFLAGS   = -std=c99 -pedantic -Wall -Wextra -Wno-unused-parameter -Os
 LDFLAGS  = -L$(X11LIB) -s
 # OpenBSD: add -lsndio
 # FreeBSD: add -lkvm -lsndio
-LDLIBS   = -lX11
+LDLIBS   = -lX11 -lkvm -lsndio
 
 # compiler and linker
 CC = cc
